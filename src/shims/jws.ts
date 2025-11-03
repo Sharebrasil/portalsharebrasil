1ef4fc6ddc3caa48da965aebd8bef178d7a5e58d
Sharebrasil <sharebrasilmt@gmail.com>
2025-11-03 15:17:44 -0300

    Delete src/shims directory

--- src/shims/jws.ts
+++
@@ -1,2 +0,0 @@
-// jws is a dependency of jsonwebtoken; provide a minimal shim for the browser
-export default {};
--- src/shims/safe-buffer.ts
+++
@@ -1,8 +0,0 @@
-// Provide a minimal safe-buffer shim that throws a clear error when used in the browser
-export const Buffer = {
-  from() {
-    throw new Error('Buffer.from is not available in the browser in this app. JWT signing should run on the server.');
-  }
-};
-
-export default { Buffer };
